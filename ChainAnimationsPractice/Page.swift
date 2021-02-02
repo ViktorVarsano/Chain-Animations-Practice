15af7ded13efc578c8b1b2ee2cb86251aad7863c
Viktor Varsano <Viktor_varsano@hotmail.com>
2021-02-02 10:14:41 +0200

    modify text and layout of three views in a new page controller, refactor storyboards

---
+++ ChainAnimationsPractice/Page.swift
@@ -0,0 +1,13 @@
+//
+//  Page.swift
+//  ChainAnimationsPractice
+//
+//  Created by Viktor Varsano on 2.02.21.
+//
+
+import Foundation
+
+struct Page {
+    let title: String
+    let body: String
+}
